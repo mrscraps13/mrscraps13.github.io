9be5003f88b9ae873cb100a114533f5321330a3c
mrscraps13 <73395545+mrscraps13@users.noreply.github.com>
2022-04-06 11:24:27 -0500

    removing button

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ if (visitCount) {
 counterContainer.innerHTML = visitCount;
 
 // Adding onClick event listener
-resetButton.addEventListener("click", () => {
-  visitCount = 1;
-  localStorage.setItem("page_view", 1);
-  counterContainer.innerHTML = visitCount;
-});
+//resetButton.addEventListener("click", () => {
+//  visitCount = 1;
+//  localStorage.setItem("page_view", 1);
+//  counterContainer.innerHTML = visitCount;
+//});
